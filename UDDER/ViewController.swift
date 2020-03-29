b091e96a47ad6da5c175a431051022406eafcc19
Nikita <nikvel1995@gmail.com>
2020-03-29 13:24:01 +0100

    Initial Commit

---
+++ UDDER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UDDER
+//
+//  Created by Nikita on 29/03/2020.
+//  Copyright © 2020 Nikita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
